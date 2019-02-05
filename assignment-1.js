40f93365e5f4594a0e904cb1e74247c16a28d6ca
Shitong Liu <sunnyliuuw@gmail.com>
2019-02-05 03:43:18 -0800

    assignment 3-1 is done

---
+++ assignment-1.js
@@ -0,0 +1,8 @@
+let arr = ['Assignment', 'is', 'due', 'on', 'Tuesday'];
+console.log(arr);
+arr.push('noon');
+console.log(arr);
+arr.splice(2, 1);
+console.log(arr);
+let str = arr.join(',');
+console.log(str);
